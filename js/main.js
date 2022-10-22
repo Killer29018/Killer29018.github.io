e18dc0e2fbcc652968bf9514ab0052cda47f407f
Aaron Danton <aarondanton04@hotmail.com>
2022-10-22 23:23:31 +0100

    Updated sections to be dynamic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function scrollToID(id)
 {
-    document.getElementById(id).scrollIntoView();
+    // document.getElementById(id).scrollIntoView({block: "start", behaviour: "smooth", inline: "start"});
+    document.getElementById(id).scrollIntoView(true);
 }
