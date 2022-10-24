908e4ba522c04fcb0a1fc7634fc91a170caf62a8
Aaron Danton <aarondanton04@hotmail.com>
2022-10-24 20:12:48 +0100

    Started adding projects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
-function scrollToID(id)
+// function scrollToID(id)
+// {
+//     document.getElementById(id).scrollIntoView(true);
+// }
+
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState == "interactive")
+    {
+        populateProjects();
+    }
+})
+
+function redirect(url)
 {
-    // document.getElementById(id).scrollIntoView({block: "start", behaviour: "smooth", inline: "start"});
-    document.getElementById(id).scrollIntoView(true);
+    window.open(url, 'mywindow');
+}
+
+function populateProjects()
+{
+    var projectsData = document.getElementById("ProjectsData");
+    const allProjects = projects["projects"];
+    console.log(projectsData);
+    console.log(allProjects);
+    console.log(allProjects.length);
+
+    for (var i = 0; i < allProjects.length; i++)
+    {
+        const project = allProjects[i];
+        var topDiv = document.createElement("div");
+        topDiv.setAttribute("onclick", `redirect('${allProjects[i]['link']}')`);
+
+        var name = document.createElement("div");
+        name.setAttribute("id", "Name");
+        name.innerText = allProjects[i]['name'];
+
+        var image = document.createElement("img");
+        image.setAttribute("src", `${allProjects[i]['img']}`);
+
+        var desc = document.createElement("div");
+        desc.setAttribute("id", "Desc");
+        desc.innerText = allProjects[i]['description'];
+
+        topDiv.appendChild(name);
+        topDiv.appendChild(image);
+        topDiv.appendChild(desc);
+
+        console.log(topDiv);
+
+        projectsData.appendChild(topDiv);
+    }
+
+    console.log(projectsData);
 }
---
+++ js/projects.js
@@ -0,0 +1,51 @@
+projects = {
+    "projects": [
+        {
+            "name": "Compute Shader raytracer",
+            "img": "assets/images/images.jpg",
+            "link": "https://github.com/Killer29018/ComputeShaderRaytracer",
+            "description": "A compute shader raytracer based on the raytracer from the \
+            'raytracing in a weekend' series by Peter Shirley"
+        },
+        {
+            "name": "Concat",
+            "img": "assets/images/images.jpg",
+            "link": "https://github.com/Killer29018/Concat",
+            "description": "A stack based programming language similar to forth that uses \
+            the reverse polish notation for instructions"
+        },
+        {
+            "name": "Rubik Cube Solver",
+            "img": "assets/images/images.jpg",
+            "link": "https://github.com/Killer29018/RubikCubeSolver",
+            "description": "A rubik cube solver that uses the beginners method to solve the cube \
+            while also outputting the moves"
+        },
+        {
+            "name": "LED Controller",
+            "img": "assets/images/images.jpg",
+            "link": "https://github.com/Killer29018/LEDController",
+            "description": "An LED Controller that can apply affects and colour palettes to an \
+            LED matrix that can receive data through WiFi"
+        },
+        {
+            "name": "Wallpaper",
+            "img": "assets/images/images.jpg",
+            "link": "https://github.com/Killer29018/PeacefulWallpaper-WallpaperEngine",
+            "description": "A dynamic wallpaper that changes with the type of dayfor use with \
+            Wallpaper Engine"
+        },
+    ]
+
+
+    // Current public Repos
+    // Raytracer: https://github.com/Killer29018/ComputeShaderRaytracer
+    // Concat: https://github.com/Killer29018/Concat
+    // Rubik Cube Solver: https://github.com/Killer29018/RubikCubeSolver
+    // Led Controller: https://github.com/Killer29018/LEDController
+    // Wallpaper: https://github.com/Killer29018/PeacefulWallpaper-WallpaperEngine
+
+    // Current Private Repos
+    // TBD
+    // Slime Simulation
+};
